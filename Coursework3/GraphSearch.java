4d253c9a5e279b1ee3b030da870227f3b492204f
Junhato <junshu.song@mail.mcgill.ca>
2015-02-18 17:07:37 -0500

    added code for case n = 2, other case still dont know how

--- Coursework3/GraphSearch.java
+++ Coursework3/GraphSearch.java
@@ -1,136 +1,181 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class GraphSearch {
-
-	public GraphSearch() {
-	}
-
-	private void BubbleSort(Node[] array) {
-
-		Node temp;
-		for (int j=0; j < array.length; j++) {
-			for (int i=0; i < array.length -j - 1; i++) {
-				if (Integer.parseInt(array[i].name()) > Integer.parseInt(array[i+1].name())) {
-				temp = array[i];
-				array[i] = array[i+1];
-				array[i+1] = temp;
-				}
-			}
-		}
-	}
-	private void print(Graph graph) {
-
-		Set<Node> nodes = graph.nodes();
-
-		Node[] aCopy = nodes.toArray(new Node[nodes.size()]);
-		BubbleSort(aCopy);
-
-		for (int i = 0; i < aCopy.length; i++) {
-			Node[] Neighbours = aCopy[i].neighbours().toArray(new Node[aCopy[i].degree()]);
-			BubbleSort(Neighbours);
-			System.out.print(aCopy[i].name() + " ");
-			for (int k = 0; k < Neighbours.length; k++) {
-				System.out.print(Neighbours[k].name());
-				if(k != (Neighbours.length -1)) System.out.print(" ");
-			}
-		System.out.println("");
-
-		}
-	}
-
-	private static Set<Node> neighbourSearch(Graph graph, int n) {
-	// create the output list
-	Set<Node> output = new HashSet<Node>();
-	
-	// do the search and fill the output
-	for (Node node : graph.nodes()) {
-            if (node.degree() >= n) {
-		    output.add(node);
-	    }
-        }
-	return output;
-	}
-
-	private static boolean findneighbour(Node node, Set<Node> nodes) {
-		for (Node n : nodes) {
-			if (n.name().equals(node.name())) return true;
-		}
-		return false;
-	}
-	
-	private static Set<Node> findFullyConnectedNeighbours(Graph graph) {
-		Set<Node> output = new HashSet<Node>();
-		Iterator itrtg = graph.nodes().iterator();
-
-		while (itrtg.hasNext()) {
-		int count = 0;
-		Node anode = (Node)itrtg.next();
-
-		Iterator itrtn = anode.neighbours().iterator();
-		Node aneighbour = (Node)itrtn.next();
-			if (anode.degree() == aneighbour.degree()) {
-				while(itrtn.hasNext() && findneighbour((Node)itrtn.next(), aneighbour.neighbours())) {
-					count = count + 1;
-				}
-				if (count == (anode.neighbours().size() - 1)) {
-					output.add(anode);
-				}
-			}
-		}
-	return output;
-	}
-
-	public static int findNumberOfCliques(Graph graph, int n) {
-		int number = 0;
-		Iterator itrtg = graph.nodes().iterator();
-
-		while (itrtg.hasNext()) {
-		int count = 0;
-		Node anode = (Node)itrtg.next();
-
-		Iterator itrtn = anode.neighbours().iterator();
-		Node aneighbour = (Node)itrtn.next();
-				while(itrtn.hasNext()) {
-					Node temp = (Node)itrtn.next();
-					if (findneighbour(temp, aneighbour.neighbours())) count = count + 1;
-				}
-				if ((count == (anode.neighbours().size() - 1)) && count >= n) {
-					number = number + 1;
-				}
-		}
-	return number;
-	}
-
-	public static void main(String[] args) {
+public class GraphSearch{
+	public static void main(String args[]){
+		GraphSearch graphsearch = new GraphSearch();
 		
-		GraphSearch test = new GraphSearch();
-		Reader reader = new Reader();
-		try { reader.read(args[1]); }
-		catch (IOException ioe) {System.err.println("invalid file");}
-		Graph graph = reader.graph();
+		String arg = args[0];
+		String file = args[1];
+		try{
+			Reader load = new Reader();
+			load.read(file);
+			Graph graph = load.graph();
+		
+			switch (arg){
+				case "-p1":
+				 graphsearch.test1(graph);
+				 break;
+				case "-p2":
+				 graphsearch.test2(graph, args[2]);
+				 break;
+				case "-p3":
+				 graphsearch.test3(graph);
+				 break;
+				case "-p4":
+				 graphsearch.test4(graph, args[2]);
+				 break;
+				default:
+				 System.out.println("The argument is not recognized");
+			 }
+		 }
+		 catch(IOException e){
+			System.err.println("IOException");
+		}
+	}
+	
+	public void test1(Graph graph){
 
-		if (args[0].equals("-p1")) {
-
-			test.print(graph);
+		//get list of nodes
+		Set<Node> nodes = graph.nodes();
+		ArrayList <Integer> nodeList = new ArrayList<Integer>();
+		//change name of node to value
+		for(Node n:nodes){
+			int node = Integer.parseInt(n.name());
+			nodeList.add(node);
+		}
+		//sort nodes
+		Collections.sort(nodeList);
+		//print for nodes to print out
+		ArrayList<String> print = new ArrayList<String>();
+		for(int e : nodeList){
+			String t = String.valueOf(e);
+			print.add(t);
+		}
+		//make string having node and neighbour		
+		for(Node node:nodes){
+			int i = 0;
+			int x = Integer.parseInt(node.name());	
+			while(x != nodeList.get(i)){
+				i++;
+			}
+			ArrayList <Integer> neighbourList = new ArrayList<Integer>();
+		
+			for(Node n:(node.neighbours())){
+				int y = Integer.parseInt(n.name());
+				neighbourList.add(y);
+			}
+			Collections.sort(neighbourList);
+			String s = new String(node.name());
+			for(int num:neighbourList){
+				s = s + " " + num;
 			}
 
-		else if (args[0].equals("-p2")) {
-
-			Set<Node> result = test.neighbourSearch(graph, Integer.parseInt(args[2]));
-			System.out.println("Number of nodes with at least " + args[2] + " neighbours: " + result.size());
+			print.set(i, s);
+			
 		}
+		//print nodes with new line
+		String a = new String();
+		for (String b:print){
+			if (b.equals(print.get(0))){
+				a = b;
+			}
+			else {
+				a = a + "\n" + b;
+			}
+		} 
+		System.out.println(a);
+	}
+		
 
-		else if (args[0].equals("-p3")) {
-
-			Set<Node> result = test.findFullyConnectedNeighbours(graph);
-			System.out.println("number of nodes with fully connected neighbours: " + result.size());
+	public static Set<Node> neighbourSearch(Graph graph, int n)
+	{
+		// create the output list
+		Set<Node> output = new HashSet<Node>();
+		
+		// get list of nodes	
+		Set<Node> node = graph.nodes();
+		
+		// do the search and fill the output
+		for (Node k:node){
+			if ((k.degree()) >= n) output.add(k);
 		}
+		
+		//
+		
+		
+		return output;
+	}
+	public void test2(Graph graph, String s){
+		try{
+			int n = Integer.parseInt(s);
 
-		else if (args[0].equals("-p4")) {
+			int num = (neighbourSearch(graph, n).size());
+			System.out.println("Number of nodes with at least " + n + " neighbours: " + num);
+		}
+		catch(Exception e){
+			System.err.println("Something wrong");
+		}
+	}
+	public static Set<Node> findFullyConnectedNeighbours(Graph graph){
+		Set<Node> output = new HashSet<Node>();
+		Set<Node> nodes = graph.nodes();
+		for(Node n:nodes){
+			Set<Node> neighbour = new HashSet<Node>(n.neighbours());
+			neighbour.add(n);
+			String tf = "t";
+			for (Node t:neighbour){
+				Set<Node> set = new HashSet<Node>(t.neighbours());
+				set.add(t);		
+				if (set.containsAll(neighbour) == false){
+					tf = "f";
+					break;
+				}
+			}
+			if(tf.equals("t")){
+				output.add(n);
+			}		
+		}
+		return output;
+	}
+	public void test3(Graph graph){
+		int c = (findFullyConnectedNeighbours(graph)).size();
+		System.out.println("Number of nodes with fully connected neighbours: " + c);
+	}
+        public static int findNumberOfCliques(Graph graph, int n) {
+		Set<Node> nodes = findFullyConnectedNeighbours(graph);
+                        Set<Node> set = new HashSet<Node>();
 
-			Set<Node> result = test.neighbourSearch(graph, Integer.parseInt(args[2]));
-			System.out.println("Number of cliques of size " + args[2] + " : " + test.findNumberOfCliques(graph, Integer.parseInt(args[2])));
+			if (n == 1) {
+			return nodes.size();
+		}
+		if (n == 2) {
+			nodes = graph.nodes();
+			int edges = 0;
+			for(Node node:nodes) {
+			edges = edges + node.degree();
+			}
+		return edges/2;
+		}
+                        for(Node k: nodes){
+                                if (k.degree() >= n-1) {
+                                        for(Node h:k.neighbours()){
+                                                set.add(h);
+                                        }
+                                        set.add(k);
+                                }
+                        }
+                        int output = set.size();
+                        return output;
+        }
+	public void test4(Graph graph, String s){
+		try{
+			int n = Integer.parseInt(s);
+			System.out.println("Number of cliques of size " + n +": " + findNumberOfCliques(graph, n));
+		}
+		catch(Exception e){
+			System.err.println("Something wrong");
 		}
 	}
 }
+			
