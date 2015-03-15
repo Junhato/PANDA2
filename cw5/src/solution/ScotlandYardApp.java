323f13dd1d50cf4de9c94906eb795756d90554a3
mami-t <mami-t@users.noreply.github.com>
2015-03-15 22:54:59 +0000

    Create ScotlandYardApp.java

---
+++ cw5/src/solution/ScotlandYardApp.java
@@ -0,0 +1,15 @@
+import solution.*;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+import java.util.*;
+
+import scotlandyard.*;
+
+public class ScotlandYardApp {
+	public static void main (String[] args) {
+		gameView view = new gameView();
+ 		gameController controller = new gameController(view);
+		view.setVisible(true);
+	}
+}
