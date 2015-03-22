ebf68aac1e8d599cf24a63e8b22d9f34e4c7d741
mami-t <mami-t@users.noreply.github.com>
2015-03-22 22:24:04 +0000

    Update gameStartController.java

--- cw5/src/gui/gameStartController.java
+++ cw5/src/gui/gameStartController.java
@@ -5,6 +5,10 @@ import java.awt.event.*;
 import java.lang.reflect.Field;
 import java.lang.*;
 import java.util.*;
+import java.util.Random;
+//import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class gameStartController {
 	private gameStart sview;
@@ -15,6 +19,11 @@ public class gameStartController {
 	private Map<Ticket, Integer> mrxTickets = new HashMap<Ticket, Integer>();
 	private Map<Ticket, Integer> detectiveTickets = new HashMap<Ticket, Integer>();
 
+	private List<Integer> locations = new LinkedList<Integer>(Arrays.asList(13,26,29,34,50,53,91,94,103,112,117,132,138,141,155,174,197,198));
+	private int startCard = 18;
+	private Random randomGenerator = new Random();
+	private int randomInt = 0;
+	private int mrxLocation;
 	public gameStartController(ScotlandYardModel model, gameStart sview) {
 		this.model = model;
 		this.sview = sview;
@@ -28,8 +37,8 @@ public class gameStartController {
 		mrxTickets.put(Ticket.Taxi, 11);
 		mrxTickets.put(Ticket.Bus, 8);
 		mrxTickets.put(Ticket.Underground, 4);
-		mrxTickets.put(Ticket.DoubleMove, 3);
-		mrxTickets.put(Ticket.SecretMove, 3);
+		mrxTickets.put(Ticket.DoubleMove, 2);
+		mrxTickets.put(Ticket.SecretMove, 5);
 
 
 		detectiveTickets.put(Ticket.Taxi, 11);
@@ -39,8 +48,33 @@ public class gameStartController {
 		this.sview.addListener(new AddListener());
 		this.sview.addStartListener(new startListener());
 		this.sview.addMapListener(new mapListener());
-		this.model.join(MrX, Colour.Black, 67, mrxTickets);
+		this.sview.addSecretListener(new secretListener());
+
+
+		randomInt = randomGenerator.nextInt(startCard);
+		this.model.join(MrX, Colour.Black, locations.get(randomInt), mrxTickets);
+		mrxLocation = locations.get(randomInt);
+		locations.remove(randomInt);
+		startCard -= 1;
 	}
+	public int getMrXLocation(){
+		return mrxLocation;
+	}
+
+class secretListener implements ActionListener{
+	public void actionPerformed(ActionEvent actionEvent){
+		String show = "MrX's Starting position:" + mrxLocation;
+		JTextField field = new JTextField();
+		field.setText(show);
+		field.setEditable(false);
+
+		JFrame location = new JFrame();
+		location.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+ 		location.setSize(200, 100);
+		location.add(field);
+		location.setVisible(true);
+	}
+}
 
 class AddListener implements ActionListener {
 	public void actionPerformed(ActionEvent actionEvent) { 
@@ -58,17 +92,21 @@ class AddListener implements ActionListener {
 		catch(IllegalAccessException e){
 			System.err.println(e);
 		}
+		randomInt = randomGenerator.nextInt(startCard);
 
-		boolean state = model.join(players.get(playerNumber), colour, sview.getStartLocation(), detectiveTickets);
+		boolean state = model.join(players.get(playerNumber), colour, locations.get(randomInt), detectiveTickets);
+
 		if(state == true){
 		String playercolour = sview.getColour();
-		String playerlocation = String.valueOf(sview.getStartLocation());
+		String playerlocation = String.valueOf(locations.get(randomInt));
 		sview.addText(playercolour, playerlocation);
 		}
 		if(model.isReady() == true){
 		sview.addStart();
 		sview.removeAddButton();
 		}
+		locations.remove(randomInt);
+
 
 	}
 
