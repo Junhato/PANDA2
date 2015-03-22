08c7c854a51160881b66457a0d81def4ceef0e59
mami-t <mami-t@users.noreply.github.com>
2015-03-22 22:22:28 +0000

    Update gameStart.java

--- cw5/src/gui/gameStart.java
+++ cw5/src/gui/gameStart.java
@@ -45,4 +45,108 @@ public class gameStart extends JFrame{
 		
 		locationField.setText("");
 		locationField.setMaximumSize(new Dimension(Integer.MAX_VALUE, locationField.getPreferredSize().height) );
-		locati
+		locationField.setFont(font2);
+		locationField.setEditable(false);
+
+		addButton = new JButton("add");
+		startButton = new JButton("start");
+		mapButton = new JButton("map");
+
+		comboPanel = new JPanel();
+		addCombobox();
+		comboPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, comboPanel.getPreferredSize().height) );
+
+		playerPanel = new JPanel();
+		playerPanel.setLayout(new FlowLayout());
+		playerPanel.add(new JLabel("Player:"));
+
+		playerPanel.add(colourField);
+		playerPanel.add(new JLabel("Location:"));
+
+		playerPanel.add(locationField);
+		playerPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, playerPanel.getPreferredSize().height) );
+
+		fieldPanel = new JPanel();
+		fieldPanel.setLayout(new BoxLayout(fieldPanel, BoxLayout.PAGE_AXIS));
+		fieldPanel.add(blackPanel);
+		fieldPanel.add(playerPanel);
+		fieldPanel.add(comboPanel);
+		fieldPanel.add(addButton);
+		fieldPanel.add(mapButton);
+
+		this.setBackground(Color.WHITE);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 		this.setSize(1200, 900);
+		//this.setLayout(new BorderLayout());
+		this.add(fieldPanel);
+	}
+	public void addCombobox(){
+	//comboPanel.removeAll();
+
+    colourCombo = new JComboBox<String>(colours);
+    colourCombo.setPreferredSize(new Dimension(80, 30));
+
+    /*locationCombo = new JComboBox<Integer>(locations);
+    locationCombo.setPreferredSize(new Dimension(80, 30));*/
+
+
+    //JPanel comboPanel = new JPanel();
+    comboPanel.add(new JLabel("Player:"));
+	comboPanel.add(colourCombo);
+    //comboPanel.add(new JLabel("  Location:"));
+    //comboPanel.add(locationCombo);
+
+    /*label = new JLabel();
+    JPanel labelPanel = new JPanel();
+    labelPanel.add(label);
+
+    getContentPane().add(p, BorderLayout.CENTER);
+    getContentPane().add(labelPanel, BorderLayout.PAGE_END);*/
+  }
+	public void addSecretListener (ActionListener listenSecretButton){
+		secretButton.addActionListener(listenSecretButton);
+	}
+
+	public void addListener(ActionListener listenAddButton) {
+
+		addButton.addActionListener(listenAddButton);
+	} 
+	
+	public void addStart(){
+		fieldPanel.add(startButton);
+	}
+	
+	public void addStartListener(ActionListener listenStartButton){
+		startButton.addActionListener(listenStartButton);
+	}
+
+	public void addMapListener(ActionListener listenMapButton){
+		mapButton.addActionListener(listenMapButton);
+	}
+
+	public String getColour(){
+		return (String)colourCombo.getSelectedItem();
+	} 
+	/*public int getStartLocation(){
+		return(int)locationCombo.getSelectedItem();
+	}*/
+	public void addText(String colour, String location){
+		colourField.append("\n" + colour);
+		colourField.setMaximumSize(new Dimension(Integer.MAX_VALUE, colourField.getPreferredSize().height) );
+		
+		locationField.append("\n" + location);
+		locationField.setMaximumSize(new Dimension(Integer.MAX_VALUE, locationField.getPreferredSize().height) );
+		playerPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, playerPanel.getPreferredSize().height) );
+
+		//colours.remove(colour);
+		//locations.remove(location);
+	}
+	public void disapper(){
+		this.setVisible(false);
+	}
+	public void removeAddButton(){
+		fieldPanel.remove(addButton);
+	}
+		
+
+}
