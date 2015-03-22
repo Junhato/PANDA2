881556b9dfaf584d2d3cb8f2d5beb3c1725d6a41
mami-t <mami-t@users.noreply.github.com>
2015-03-22 22:19:20 +0000

    Update setNumberPlayer.java

--- cw5/src/gui/setNumberPlayer.java
+++ cw5/src/gui/setNumberPlayer.java
@@ -1,3 +1,9 @@
+package solution;
+
+import java.awt.*;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.imageio.*;
 import java.io.*;
 import java.lang.*;
 import java.awt.image.BufferedImage;
@@ -6,40 +12,37 @@ public class setNumberPlayer extends JFrame{
 	private JComboBox<Integer> numberField;
 	private Integer[] numbers = {2, 3, 4, 5};
 	private JButton numberButton;
+	private JButton loadButton;
 
 	public setNumberPlayer(){
 		numberField = new JComboBox<Integer>(numbers);
 		//numberField.setMaximumSize(new Dimension(100, 20) );
 		
-		numberButton = new JButton("next");
+		numberButton = new JButton("New Game");
 		
 		JPanel numberPanel = new JPanel();
-		numberPanel.setLayout(new FlowLayout());
-		numberPanel.setMinimumSize(new Dimension(500, 200));
+		//numberPanel.setLayout(new FlowLayout());
+		/*numberPanel.setMinimumSize(new Dimension(500, 200));
 		numberPanel.add(new JLabel("How many are playing?"));
 		numberPanel.add(numberField);
-		numberPanel.add(numberButton);
+		numberPanel.add(numberButton);*/
+		JPanel comboPanel = new JPanel();
+		comboPanel.setMaximumSize(new Dimension(300, 50));
+
+		loadButton = new JButton("Load privious game");
 
 		this.setBackground(Color.WHITE);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
  		//this.setSize(1200, 900);
-		this.setSize(800, 500);
+		this.setSize(300, 200);
 
-		//this.setLayout(new FlowLayout());
+		numberPanel.setLayout(new BoxLayout(numberPanel, BoxLayout.PAGE_AXIS));
+
 		//this.add(new JLabel("How many are playing?"));
 		
-		this.add(numberPanel);
-		//this.add(numberButton);
-		
+		numberPanel.setMinimumSize(new Dimension(300, 200));
+		numberField.setMaximumSize(new Dimension(100, 50));
 
-	}
-
-	public int getNumber(){
-		return (int) numberField.getSelectedItem();
-	}
-
-	public void addNumberListener(ActionListener listenNumberButton) {
-
-		numberButton.addActionListener(listenNumberButton);
-	} 
-}
+		numberPanel.add(new JLabel("How many are playing?"));
+		comboPanel.add(numberField);
+		numbe
