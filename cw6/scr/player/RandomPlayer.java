2a9946344482423d99f860d32b426112fee65778
mami-t <mami-t@users.noreply.github.com>
2015-04-10 19:01:35 +0100

    i dont know how i can get current location of black...

--- cw6/scr/player/RandomPlayer.java
+++ cw6/scr/player/RandomPlayer.java
@@ -22,28 +22,23 @@ import java.util.Set;
 public class RandomPlayer implements Player{
 	protected ScotlandYardView view;
 	protected String graphFilename;
-	protected Map<Move, Integer> scores = new HashMap<Move, Integer>();
 	protected Map<Integer, List<Integer>> coordinateMap = new HashMap<Integer, List<Integer>>();
 	protected Graph currentGraph;
 	
-    public RandomPlayer(ScotlandYardView view, String graphFilename) throws IOException{
+    public RandomPlayer(ScotlandYardView view, String graphFilename){
         //TODO: A better AI makes use of `view` and `graphFilename`.
         this.view = view;
         this.graphFilename = graphFilename;
         readFile();
         ScotlandYardGraphReader Reader = new ScotlandYardGraphReader();
-		this.currentGraph = Reader.readGraph(graphFilename);
+		//this.currentGraph = Reader.readGraph(graphFilename);
         
     }
 
     @Override
     public Move notify(int location, Set<Move> moves) {
         //TODO: Some clever AI here ...
-		Colour player = view.getCurrentPlayer();
-		/*if(player.equals(Colour.Black)){
-			Move choosenMove = score(moves);
-			if(choosenMove != null) return choosenMove;
-		}*/
+		score(moves);
         int choice = new Random().nextInt(moves.size());
         for (Move move : moves) {
             if (choice == 0) {
@@ -54,21 +49,15 @@ public class RandomPlayer implements Player{
 
         return null;
     }
-    //AI for MrX
-    public Move score(Set<Move> moves){
-		List<Colour> players= view.getPlayers();
-		
-		for(Move move:moves){
-			scores.put(move, 0);
-		}
-		
-		distance(moves, players);
-		freedom(moves);
-		overall(moves);
-		
-        Move choosenMove = rankMove(moves);
-        System.out.println(choosenMove);
-        return choosenMove;
+    //print current board score
+    public void score(Set<Move> moves){
+		Colour player = view.getCurrentPlayer();
+		List<Colour> players = view.getPlayers();
+		int score = 0;
+		score += distance(moves, player, players);
+		score += freedom(moves);
+		score += overall(moves, player);
+        System.out.println(score);
 	}
 	//read pos.text
     public void readFile(){
@@ -106,94 +95,47 @@ public class RandomPlayer implements Player{
 				coordinateMap.put(key, pos);
 			}
 	}
-	//get target of move
-	public int targetPos(Move move){
-		int target = 0;
-			if (move instanceof MoveTicket) {
-				target = ((MoveTicket) move).target;
-			}
-			else if(move instanceof MoveDouble){
-				target = ((MoveDouble) move).move2.target;
-			}
-			else{
-				target = view.getPlayerLocation(Colour.Black);
-			}
-		return target;
-	}
-    //measure distance to each ditective
-    //take the shortest distance 
-    public void distance(Set<Move> moves, List<Colour> players){
-		for(Move move:moves){
-			int target = targetPos(move);
-			List<Integer> pos = coordinateMap.get(target);
-			int distance = scores.get(move);
 
+    //distance to ditectives
+    public int distance(Set<Move> moves, Colour currentPlayer, List<Colour> players){
+		int currentLocation = view.getPlayerLocation(currentPlayer);
+		List<Integer> pos = coordinateMap.get(currentLocation);
+		int score = 0;
 			for(Colour player:players){
 				int location = view.getPlayerLocation(player);
 				List<Integer> co = coordinateMap.get(location);
-				if(co != null){
+				if(co != null && pos != null){
 					double value = Math.sqrt((pos.get(0)-co.get(0))*(pos.get(0)-co.get(0))+(pos.get(1)-co.get(1))*(pos.get(1)-co.get(1)));
-					distance += (int) value;
-					if(move instanceof MoveDouble){
-						distance = distance * 2/3;
-					}
-					if(distance != 0 && (scores.get(move) == 0 || distance < scores.get(move))){
-						scores.put(move, distance);
-					}
+					score += (int) value;
+					
+					
 				}
 			}
-		
-		}
+		return score;
 	}
 	//freedom of move
-	//count how many possible moves in next turn
-	public void freedom(Set<Move> moves){
-		for (Move move:moves){
-			int target = targetPos(move);
-			
-			Set<Edge> edges = currentGraph.getEdges(target);
-			int score = 0;
-			for(Edge edge:edges){
-				Ticket ticket = Ticket.fromRoute((Route)edge.data());
-				if(view.getPlayerTickets(Colour.Black, ticket) != 0) score += 1;
-				
-			}
-			score += scores.get(move);
-			scores.put(move, score);
-		}
+	public int freedom(Set<Move> moves){
+		return moves.size();
 	}
 	//overall position on the board
-	//check how close to the corner
-	public void overall(Set<Move> moves){
-		for (Move move:moves){
-			int target = targetPos(move);
-			List<Integer> pos = coordinateMap.get(target);
+	public int overall(Set<Move> moves, Colour player){
+			int location = view.getPlayerLocation(player);
+			List<Integer> pos = coordinateMap.get(location);
 			int score = 0;
-			int upLeft = pos.get(0) + pos.get(1);
-			int downLeft = pos.get(0) + 800 - pos.get(1);
-			int upRight = 1000 - pos.get(0) + pos.get(1);
-			int downRight = 1000 - pos.get(0) + 800 - pos.get(1);
-			score = upLeft;
-			if(score > downLeft) score = downLeft;
-			if(score > upRight) score = upRight;
-			if(score > downRight) score = downRight;
-			score = score/2;
-			score += scores.get(move);
-			scores.put(move, score);
-		}
-	}
-	//choose move with highest score
-	public Move rankMove(Set<Move> moves){
-		Move choice = null;
-		int score = 0;
-		for(Move move:moves){
-			if(scores.get(move) > score){
-				choice = move;
-				score = scores.get(move);
+			if(pos != null){
+				int upLeft = pos.get(0) + pos.get(1);
+				int downLeft = pos.get(0) + 800 - pos.get(1);
+				int upRight = 1000 - pos.get(0) + pos.get(1);
+				int downRight = 1000 - pos.get(0) + 800 - pos.get(1);
+			
+				score = upLeft;
+				if(score > downLeft) score = downLeft;
+				if(score > upRight) score = upRight;
+				if(score > downRight) score = downRight;
 			}
-		}
-		return choice;
+			return score;
 	}
+	
 	
 			
 		
